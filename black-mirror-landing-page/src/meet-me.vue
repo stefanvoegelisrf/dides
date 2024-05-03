78e9052413b24ca7c41f1b9473c57359fea9b018
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-05-03 11:37:25 +0200

    initial meet me, style start page

--- black-mirror-landing-page/src/meet-me.vue
+++ black-mirror-landing-page/src/meet-me.vue
@@ -1,6 +1,120 @@
 <template>
-
+    <main class="h-screen bg-dark-gunmetal">
+        <div id="beach-scene" class="rounded-full absolute top-1/2 -translate-y-1/2 -left-40">
+            <div class="relative w-full h-full">
+                <div class="circle absolute top-0 left-0  h-full w-full" v-for="n in 18">
+                    <div class="relative h-full w-full">
+                        <span
+                            class="absolute left-1/2 -translate-x-1/2 top-0 translate-y-1/2 w-24 h-24 bg-dark-gunmetal rounded-full"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="nanum-myeongjo text-white">“If we really met, you wouldn't like me.”</p>
+        <h1 class="m-plus-1p-medium color-tuscany">SAN JUNIPERO</h1>
+        <h2 class="m-plus-1p-regular color-tuscany">Black Mirror Series 3, Episode 4</h2>
+        <h2 class="m-plus-1p-regular color-tuscany">Directed by Owen Harris</h2>
+    </main>
 </template>
 <style scoped>
+main {
+    --color-dark-gunmetal: #1D2734;
+    --color-tuscany: #BD98A0;
+}
 
+#beach-scene {
+    background-image: url('/images/meet-me/vlcsnap-2024-05-01-15h22m58s753.png');
+    background-size: cover;
+    background-position: 35%;
+    height: 120vmin;
+    width: 120vmin;
+}
+
+.circle:nth-child(1) {
+    transform: rotate(0deg);
+}
+
+.circle:nth-child(2) {
+    transform: rotate(20deg);
+}
+
+.circle:nth-child(3) {
+    transform: rotate(40deg);
+}
+
+.circle:nth-child(4) {
+    transform: rotate(60deg);
+}
+
+.circle:nth-child(5) {
+    transform: rotate(80deg);
+}
+
+.circle:nth-child(6) {
+    transform: rotate(100deg);
+}
+
+.circle:nth-child(7) {
+    transform: rotate(120deg);
+}
+
+.circle:nth-child(8) {
+    transform: rotate(140deg);
+}
+
+.circle:nth-child(9) {
+    transform: rotate(160deg);
+}
+
+.circle:nth-child(10) {
+    transform: rotate(180deg);
+}
+
+.circle:nth-child(11) {
+    transform: rotate(200deg);
+}
+
+.circle:nth-child(12) {
+    transform: rotate(220deg);
+}
+
+.circle:nth-child(13) {
+    transform: rotate(240deg);
+}
+
+.circle:nth-child(14) {
+    transform: rotate(260deg);
+}
+
+.circle:nth-child(15) {
+    transform: rotate(280deg);
+}
+
+.circle:nth-child(16) {
+    transform: rotate(300deg);
+}
+
+.circle:nth-child(17) {
+    transform: rotate(320deg);
+}
+
+.circle:nth-child(18) {
+    transform: rotate(340deg);
+}
+
+.color-dark-gunmetal {
+    color: var(--color-dark-gunmetal);
+}
+
+.color-tuscany {
+    color: var(--color-tuscany);
+}
+
+.bg-dark-gunmetal {
+    background-color: var(--color-dark-gunmetal);
+}
+
+.bg-tuscany {
+    background-color: var(--color-tuscany);
+}
 </style>
--- black-mirror-landing-page/src/start-page.vue
+++ black-mirror-landing-page/src/start-page.vue
@@ -1,16 +1,79 @@
 <template>
     <main class="flex flex-col items-center h-screen bg-no-repeat bg-center bg-cover">
         <section id="header" class="w-max">
-            <h1 class="m-plus-1p-medium text-8xl p-8">San Junipero</h1>
+            <h1 class="m-plus-1p-medium text-8xl p-8 color-midnight-green neon-pulsating-text">San Junipero</h1>
         </section>
         <section id="navigation" class="w-max grid grid-cols-2 grid-rows-1">
-            <RouterLink to="/girlfriend-in-a-coma" class="p-8">Girlfriend in a coma</RouterLink>
-            <RouterLink to="/meet-me" class="p-8">Meet me</RouterLink>
+            <RouterLink to="/girlfriend-in-a-coma" class="p-8 nanum-myeongjo text-xl text-white">Girlfriend in a coma</RouterLink>
+            <RouterLink to="/meet-me" class="p-8 nanum-myeongjo text-xl text-white">Meet me</RouterLink>
         </section>
     </main>
 </template>
 <style scoped>
 main {
     background-image: url('/images/ready/vlcsnap-2024-05-01-15h19m09s485.png');
+    --color-midnight-green: #024959;
+    --color-keppel:#3CA6A6;
+}
+
+.color-midnight-green {
+    color: var(--color-midnight-green);
+}
+
+.neon-flickering-text {
+  animation: flicker 1.5s infinite alternate;
+}
+
+.neon-pulsating-text {
+  animation: pulsate 2.5s infinite alternate;
+}
+
+/* Flickering animation */
+@keyframes flicker {
+    
+  0%, 18%, 22%, 25%, 53%, 57%, 100% {
+
+      text-shadow:
+      0 0 4px #fff,
+      0 0 11px #fff,
+      0 0 19px #fff,
+      0 0 40px var(--color-keppel),
+      0 0 80px var(--color-keppel),
+      0 0 90px var(--color-keppel),
+      0 0 100px var(--color-keppel),
+      0 0 150px var(--color-keppel);
+  
+  }
+  
+  20%, 24%, 55% {        
+      text-shadow: none;
+  }    
+}
+
+@keyframes pulsate {
+  100% {
+    /* Larger blur radius */
+    text-shadow:
+      0 0 4px #fff,
+      0 0 11px #fff,
+      0 0 19px #fff,
+      0 0 40px var(--color-keppel),
+      0 0 80px var(--color-keppel),
+      0 0 90px var(--color-keppel),
+      0 0 100px var(--color-keppel),
+      0 0 150px var(--color-keppel);
+  }
+  0% {
+    /* Smaller blur radius */
+    text-shadow:
+      0 0 2px #fff,
+      0 0 4px #fff,
+      0 0 6px #fff,
+      0 0 10px var(--color-keppel),
+      0 0 45px var(--color-keppel),
+      0 0 55px var(--color-keppel),
+      0 0 70px var(--color-keppel),
+      0 0 80px var(--color-keppel);
+  }
 }
 </style>
